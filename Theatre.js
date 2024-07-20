9b57455f6b30075171e0407e367df21e4aa4b04e
RANJITH V <ranjithsvhpc1234@gmail.com>
2024-07-20 14:21:43 +0530

    c1

---
+++ Theatre.js
@@ -0,0 +1,18 @@
+let call = () => {
+    document.getElementById("login").style.display = "block"
+
+}
+let book = () => {
+    document.getElementById("tbook").style.display = "block"
+}
+let pay = () => {
+    alert("your Tickets is Arriving From Your Gmail")
+    document.getElementById("tbook").style.display = "none"
+}
+let pro = () => {
+    document.getElementById("lsbt").style.display = "none"
+    alert("You Are Now 7Screen User")
+    document.getElementById("login").style.display = "none"
+    document.getElementById("tname").style.marginRight = "0"
+
+}
